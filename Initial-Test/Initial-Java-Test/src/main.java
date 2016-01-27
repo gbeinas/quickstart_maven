e7702506376719f516366a46ecf0cf65cf916b65
gbeinas <gbeinas@gmail.com>
2016-01-27 16:39:31 +0200

    changes

--- Initial-Test/Initial-Java-Test/src/main.java
+++ Initial-Test/Initial-Java-Test/src/main.java
@@ -5,6 +5,7 @@ public class main {
 		// TODO Auto-generated method stub
 		test_print test = new test_print();
 		System.out.print(test.test_print_("John"));
+		System.out.print("Adding a change to git again");
 	}
 
 }
