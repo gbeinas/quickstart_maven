8f9da6475923ff8a3c75a6036ec140a676452ee2
gbeinas <gbeinas@gmail.com>
2016-01-27 17:59:20 +0200

    final changes

--- Initial-Test/Initial-Java-Test/src/main.java
+++ Initial-Test/Initial-Java-Test/src/main.java
@@ -6,6 +6,7 @@ public class main {
 		test_print test = new test_print();
 		System.out.print(test.test_print_("John"));
 		System.out.print("Adding a change to git again");
+		System.out.print("Adding a change to git again,final step");
 	}
 
 }
