15d0b49552c2a0f7e108fa2d2b269be899fe4cbf
gbeinas <gbeinas@gmail.com>
2016-01-27 18:14:11 +0200

    test commit

--- Initial-Test/Initial-Java-Test/src/main.java
+++ Initial-Test/Initial-Java-Test/src/main.java
@@ -6,7 +6,6 @@ public class main {
 		test_print test = new test_print();
 		System.out.print(test.test_print_("John"));
 		System.out.print("Adding a change to git again");
-		System.out.print("Adding a change to git again,final step");
 	}
 
 }
